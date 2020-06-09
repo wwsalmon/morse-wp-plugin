75eb5f37ef5e255384dc14c523f91e666a8f310e
Samson Zhang <wwsamson@yahoo.com>
2020-06-09 16:34:11 -0400

    initial commit

---
+++ morse-wp-plugin-plugin.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Morse WordPress Theme Settings Plugin
+ * Description: Settings used in the Morse WordPress theme.
+ * Version: 0.0.1
+ * Author: Samson Zhang
+ * Author URI: https://www.samsonzhang.com/
+ */
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Morse WP Plugin
+
+Plugin with custom settings for [Morse WP Theme](https://github.com/wwsalmon/morse-wp-theme)
