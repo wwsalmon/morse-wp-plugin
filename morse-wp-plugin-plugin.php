9e86ecbe6e0a652ce26552f6250874c00e0658d6
Samson Zhang <wwsamson@yahoo.com>
2020-06-10 22:22:55 -0400

    Bump version to 0.1.0

--- morse-wp-plugin-plugin.php
+++ morse-wp-plugin-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Morse WordPress Theme Settings Plugin
  * Description: Settings used in the Morse WordPress theme.
- * Version: 0.0.1
+ * Version: 0.1.0
  * Author: Samson Zhang
  * Author URI: https://www.samsonzhang.com/
  */
